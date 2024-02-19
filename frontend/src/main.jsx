50823832ee1d12d34284043aed6538afc653d14f
stepweaver <weaverst@mail.gvsu.edu>
2024-02-19 09:04:05 -0500

    Revert "Add React Query and Devtools"
    This reverts commit 94daad12974c7d93fa98d4aa510db92c55773a25.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-//! Create a client
-const queryClient = new QueryClient();
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
