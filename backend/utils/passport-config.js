8e4b52c7ad6f451637ba1e2b5c5e2789df7e8b8a
stepweaver <weaverst@mail.gvsu.edu>
2024-02-25 09:48:25 -0500

    Add Google OAuth2.0 and JWT authentication

--- backend/utils/passport-config.js
+++ backend/utils/passport-config.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 const User = require('../models/User/User');
 const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 //! Configure passport local strategy
 passport.use(
@@ -28,6 +31,36 @@ passport.use(
       }
     }
   )
+);
+
+//! Google OAuth2.0
+//! JWT-Options
+const options = {
+  jwtFromRequest: ExtractJWT.fromExtractors([
+    (req) => {
+      let token = null;
+      if (req && req.cookies) {
+        token = req.cookies['token'];
+        return token;
+      }
+    }
+  ]),
+  secretOrKey: process.env.JWT_SECRET
+};
+//! JWT
+passport.use(
+  new JWTStrategy(options, async (userDecoded, done) => {
+    try {
+      const user = await User.findById(userDecoded.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
 );
 
 module.exports = passport;
