ab8b6dc2dfb31f6356c3fd51d2530a663af8a935
stepweaver <weaverst@mail.gvsu.edu>
2024-02-26 16:29:52 -0500

    Refactor AuthCheckingSpinner component

--- frontend/src/components/AuthRoute/AuthCheckingSpinner.jsx
+++ frontend/src/components/AuthRoute/AuthCheckingSpinner.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner } from 'react-icons/fa';
 
 const AuthLoader = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-96 bg-white">
-      <FaSpinner className="animate-spin text-4xl text-blue-500" />
-      <p className="mt-4 text-lg text-gray-900">
+    <div className='flex flex-col items-center justify-center h-96 bg-white'>
+      <FaSpinner className='animate-spin text-4xl text-blue-500' />
+      <p className='mt-4 text-lg text-gray-900'>
         Checking authentication status, please wait...
       </p>
     </div>
