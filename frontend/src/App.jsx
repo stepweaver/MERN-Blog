79b9c3c85f736facfca47644a632d6594e1fe477
stepweaver <weaverst@mail.gvsu.edu>
2024-02-26 13:12:57 -0500

    Add private navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreatePost from './components/Posts/CreatePost';
 import PostsList from './components/Posts/PostsList';
 import PublicNavbar from './components/Navbar/PublicNavbar';
+import PrivateNavbar from './components/Navbar/PrivateNavbar';
 import Home from './components/Home/Home';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
 // import UpdatePost from './components/Posts/UpdatePost';
 import PostDetails from './components/Posts/PostDetails';
 import Profile from './components/User/Profile';
+import { useSelector } from 'react-redux';
 
 function App() {
+  // Get the user from the local storage
+  const { userAuth } = useSelector((state) => state.auth);
+  console.log(userAuth);
   return (
     <BrowserRouter>
-      {/* public navbar */}
-      <PublicNavbar />
+      {userAuth ? <PrivateNavbar /> : <PublicNavbar />}
+      {/* routes */}
       <Routes>
         {/* create post */}
         <Route element={<Home />} path='/' />
