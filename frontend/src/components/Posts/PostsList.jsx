1457ee422355fce948d3dd2774a6855136520a1e
stepweaver <weaverst@mail.gvsu.edu>
2024-02-29 14:42:55 -0500

    Add search feature to PostsList component

--- frontend/src/components/Posts/PostsList.jsx
+++ frontend/src/components/Posts/PostsList.jsx
@@ -7,6 +7,8 @@ import AlertMessage from '../Alert/AlertMessage';
 import PostCategory from '../Category/PostCategory';
 import { getCategories } from '../../APIServices/categories/categoriesAPI';
 import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { MdClear } from 'react-icons/md';
 
 const PostsList = () => {
   const [filters, setFilters] = useState({});
@@ -21,6 +23,17 @@ const PostsList = () => {
 
   const handleCategoryFilter = (categoryId) => {
     setFilters({ ...filters, category: categoryId });
+    setPage(1);
+    refetch();
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setFilters({ ...filters, title: searchTerm });
     setPage(1);
     refetch();
   };
@@ -66,12 +79,39 @@ const PostsList = () => {
         <h1 className='text-4xl lg:text-6xl font-bold font-heading mb-6 mt-16'>
           Blog
         </h1>
-
         {/* featured post */}
         {/* <FeaturedPost post={featuredPost} /> */}
         <h2 className='text-4xl font-bold font-heading mb-10'>
           Latest articles
         </h2>
+        {/* Search feature */}
+        <form
+          onSubmit={handleSearchSubmit}
+          className='flex flex-col md:flex-row items-center gap-2 mb-4'
+        >
+          <div className='flex-grow flex items-center border border-gray-300 rounded-lg overflow-hidden'>
+            <input
+              type='text'
+              placeholder='Search posts...'
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className='flex-grow p-2 text-sm focus:outline-none'
+            />
+            <button
+              type='submit'
+              className='p-2 text-white bg-orange-500 hover:bg-blue-600 rounded-r-lg'
+            >
+              <FaSearch className='h-5 w-5' />
+            </button>
+          </div>
+          <button
+            // onClick={clearFilters}
+            className='p-2 text-sm text-orange-500 border border-blue-500 rounded-lg hover:bg-blue-100 flex items-center gap-1'
+          >
+            <MdClear className='h-4 w-4' />
+            Clear Filters
+          </button>
+        </form>
         {/* Post category */}
         <PostCategory
           categories={categories}
